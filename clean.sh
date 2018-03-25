42215cc1c966bcf9056758ba667ee9418be95dfb
jiangyuci <luciusjiang.vie@gmail.com>
2018-03-25 15:45:23 +1100

    .tex update

---
+++ clean.sh
@@ -0,0 +1,11 @@
+@echo off
+rm -rf  *.toc  /s
+rm -rf  *.out  /s
+rm -rf  *.aux  /s
+rm -rf  *.log  /s
+rm -rf  *.bak  /s
+rm -rf  *.bbl  /s
+rm -rf  *.blg /s
+rm -rf  *.fls  /s
+rm -rf  *.gz /s
+rm -rf  *.fdb_latexmk  /s
